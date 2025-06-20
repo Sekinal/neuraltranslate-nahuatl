12051189fed5153699b1526c05ce65ca51d831ee
thermostatic <muxxeroerty@gmail.com>
2025-06-20 07:22:42 +0000

    Init commit

---
+++ main.py
@@ -0,0 +1,100 @@
+from unsloth import FastModel
+import torch
+
+model, tokenizer = FastModel.from_pretrained(
+    model_name = "unsloth/gemma-3-4b-it",
+    max_seq_length = 2048, # Choose any for long context!
+    load_in_4bit = True,  # 4 bit quantization to reduce memory
+    load_in_8bit = False, # [NEW!] A bit more accurate, uses 2x memory
+    full_finetuning = False, # [NEW!] We have full finetuning now!
+    # token = "hf_...", # use one if using gated models
+)
+
+model = FastModel.get_peft_model(
+    model,
+    finetune_vision_layers     = False, # Turn off for just text!
+    finetune_language_layers   = True,  # Should leave on!
+    finetune_attention_modules = True,  # Attention good for GRPO
+    finetune_mlp_modules       = True,  # SHould leave on always!
+
+    r = 8,           # Larger = higher accuracy, but might overfit
+    lora_alpha = 8,  # Recommended alpha == r at least
+    lora_dropout = 0,
+    bias = "none",
+    random_state = 3407,
+)
+
+from unsloth.chat_templates import get_chat_template
+tokenizer = get_chat_template(
+    tokenizer,
+    chat_template = "gemma-3",
+)
+
+from datasets import load_dataset
+dataset = load_dataset("Thermostatic/ShareGPT-Nahuatl-Spanish", split="train")
+
+from unsloth.chat_templates import standardize_data_formats
+dataset = standardize_data_formats(dataset)
+
+def formatting_prompts_func(examples):
+   convos = examples["conversations"]
+   texts = [tokenizer.apply_chat_template(convo, tokenize = False, add_generation_prompt = False).removeprefix('<bos>') for convo in convos]
+   return { "text" : texts, }
+
+dataset = dataset.map(formatting_prompts_func, batched = True)
+
+from trl import SFTTrainer, SFTConfig
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    eval_dataset = None, # Can set up evaluation!
+    args = SFTConfig(
+        dataset_text_field = "text",
+        per_device_train_batch_size = 64,
+        gradient_accumulation_steps = 4, # Use GA to mimic batch size!
+        warmup_ratio = 0.1,
+        num_train_epochs = 1, # Set this for 1 full training run.
+        learning_rate = 2e-4, # Reduce to 2e-5 for long training runs
+        logging_steps = 1,
+        optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        report_to = "wandb", # Use this for WandB etc
+        dataset_num_proc=2,
+    ),
+)
+
+from unsloth.chat_templates import train_on_responses_only
+trainer = train_on_responses_only(
+    trainer,
+    instruction_part = "<start_of_turn>user\n",
+    response_part = "<start_of_turn>model\n",
+)
+
+trainer_stats = trainer.train()
+
+from unsloth.chat_templates import get_chat_template
+tokenizer = get_chat_template(
+    tokenizer,
+    chat_template = "gemma-3",
+)
+
+messages = [{
+    "role": "user",
+    "content": [{"type" : "text", "text" : "Traduce al español: Auh in ye yuhqui in on tlenamacac niman ye ic teixpan on motlalia ce tlacatl itech mocaua.",}]
+}]
+text = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt = True, # Must add for generation
+)
+
+from transformers import TextStreamer
+_ = model.generate(
+    **tokenizer([text], return_tensors = "pt").to("cuda"),
+    max_new_tokens = 64, # Increase for longer outputs!
+    # Recommended Gemma-3 settings!
+    temperature = 1.0, top_p = 0.95, top_k = 64,
+    streamer = TextStreamer(tokenizer, skip_prompt = True),
+)
